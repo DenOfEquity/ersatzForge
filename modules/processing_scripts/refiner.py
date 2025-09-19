b58a3ad1147525bd29df3220423c7e5197568bc7
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2025-09-19 17:49:40 +0100

    remove refiner CFG
    thought I tested this as working, but seems not
    low value anyway

--- modules/processing_scripts/refiner.py
+++ modules/processing_scripts/refiner.py
@@ -1,6 +1,6 @@
 import gradio as gr
 
-from modules import scripts, shared, sd_models, sd_samplers
+from modules import scripts, sd_models
 from modules.infotext_utils import PasteField
 from modules.ui_common import create_refresh_button
 from modules.ui_components import InputAccordion
@@ -27,10 +27,6 @@ class ScriptRefiner(scripts.ScriptBuiltinUI):
         
                 refiner_switch_at = gr.Slider(value=0.8, label="Switch at", minimum=0.01, maximum=1.0, step=0.01, elem_id=self.elem_id("switch_at"), tooltip="fraction of sampling steps when the switch to refiner model should happen; 1=never, 0.5=switch in the middle of generation")
 
-            with gr.Row():
-                refiner_cfg = gr.Slider(label='Refiner CFG', elem_id="refiner_cfg", value=0, minimum=0, maximum=16, step=0.1)
-
-
         def lookup_checkpoint(title):
             info = sd_models.get_closet_checkpoint_match(title)
             return None if info is None else info.name
@@ -39,18 +35,15 @@ class ScriptRefiner(scripts.ScriptBuiltinUI):
             PasteField(enable_refiner, lambda d: 'Refiner' in d),
             PasteField(refiner_checkpoint, lambda d: lookup_checkpoint(d.get('Refiner')), api="refiner_checkpoint"),
             PasteField(refiner_switch_at, 'Refiner switch at', api="refiner_switch_at"),
-            PasteField(refiner_cfg, 'Refiner CFG', api="refiner_cfg"),
         ]
 
-        return enable_refiner, refiner_checkpoint, refiner_switch_at, refiner_cfg
+        return enable_refiner, refiner_checkpoint, refiner_switch_at
 
-    def setup(self, p, enable_refiner, refiner_checkpoint, refiner_switch_at, refiner_cfg):
+    def setup(self, p, enable_refiner, refiner_checkpoint, refiner_switch_at):
         # the actual implementation is in sd_samplers_common.py, apply_refiner
         if not enable_refiner or refiner_checkpoint in (None, "", "None"):
             p.refiner_checkpoint = None
             p.refiner_switch_at = None
-            p.refiner_cfg = None
         else:
             p.refiner_checkpoint = refiner_checkpoint
             p.refiner_switch_at = refiner_switch_at
-            p.refiner_cfg = refiner_cfg
