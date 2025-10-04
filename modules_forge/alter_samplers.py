5f9c3c2779c31e157201e7841e293544cb60f815
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2025-10-04 01:46:20 +0100

    s_noise

--- modules_forge/alter_samplers.py
+++ modules_forge/alter_samplers.py
@@ -4,9 +4,7 @@ from backend.modules import k_diffusion_extra
 
 class AlterSampler(sd_samplers_kdiffusion.KDiffusionSampler):
     def __init__(self, sd_model, sampler_name):
-        self.sampler_name = sampler_name
-        self.unet = sd_model.forge_objects.unet
-        sampler_function = getattr(k_diffusion_extra, "sample_{}".format(sampler_name))
+        sampler_function = getattr(k_diffusion_extra, sampler_name)
         super().__init__(sampler_function, sd_model, None)
 
 
@@ -18,10 +16,13 @@ def build_constructor(sampler_name):
 
 
 samplers_data_alter = [
-    sd_samplers_common.SamplerData('DDPM', build_constructor(sampler_name='ddpm'), ['ddpm'], {}),
-    sd_samplers_common.SamplerData('SA-Solver', build_constructor(sampler_name='sa_solver'), ['sa_solver'], {}),
-    sd_samplers_common.SamplerData('Extended Reverse-Time SDE', build_constructor(sampler_name='er_sde'), ['er_sde'], {}),
-    sd_samplers_common.SamplerData('Adaptive-ODE', build_constructor(sampler_name='adaptive_ode'), ['adaptive_ode'], {}),
-    sd_samplers_common.SamplerData('Fixed-ODE', build_constructor(sampler_name='fixed_ode'), ['fixed_ode'], {}),
+    sd_samplers_common.SamplerData('DDPM', build_constructor(sampler_name='sample_ddpm'), ['ddpm'], {}),
+    sd_samplers_common.SamplerData('SA-Solver', build_constructor(sampler_name='sample_sa_solver'), ['sa_solver'], {}),
+    sd_samplers_common.SamplerData('Extended Reverse-Time SDE', build_constructor(sampler_name='sample_er_sde'), ['er_sde'], {}),
+    sd_samplers_common.SamplerData('Adaptive-ODE', build_constructor(sampler_name='sample_adaptive_ode'), ['adaptive_ode'], {}),
+    sd_samplers_common.SamplerData('Fixed-ODE', build_constructor(sampler_name='sample_fixed_ode'), ['fixed_ode'], {}),
 ]
-
+sd_samplers_kdiffusion.sampler_extra_params.update({
+    'sample_sa_solver': ['s_noise'],
+    'sample_er_sde'   : ['s_noise'],
+})
