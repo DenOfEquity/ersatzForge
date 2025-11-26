92d21aa2af955f4a665c1a9a36193af843847f42
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2025-11-26 12:56:13 +0000

    we do experiments here
    sampler 'Euler a na' (noise attenuation)
    usually functions as an LCM alternative

--- k_diffusion/sampling.py
+++ k_diffusion/sampling.py
@@ -152,11 +152,12 @@ def sample_euler(model, x, sigmas, extra_args=None, callback=None, disable=None,
 
 
 @torch.no_grad()
-def sample_euler_ancestral(model, x, sigmas, extra_args=None, callback=None, disable=None, eta=1., s_noise=1., noise_sampler=None):
+def sample_euler_ancestral(model, x, sigmas, extra_args=None, callback=None, disable=None, eta=1., s_noise=1., noise_sampler=None, noise_attenuation=False):
     """Ancestral sampling with Euler method steps."""
     extra_args = {} if extra_args is None else extra_args
     noise_sampler = default_noise_sampler(x) if noise_sampler is None else noise_sampler
     s_in = x.new_ones([x.shape[0]])
+    p = None
 
     if isinstance(model.inner_model.predictor, PredictionFlux) or isinstance(model.inner_model.predictor, PredictionDiscreteFlow):
         use_flow_method = True
@@ -182,9 +183,22 @@ def sample_euler_ancestral(model, x, sigmas, extra_args=None, callback=None, dis
             x = x + d * dt
 
         if eta > 0 and sigmas[i + 1] > 0:
-            x = alpha * x + noise_sampler(sigmas[i], sigmas[i + 1]) * s_noise * sigma_up
+            if noise_attenuation and p is not None:
+                attenuation = (x - p).abs()
+                for i in range(attenuation.shape[0]):
+                    attenuation[i] /= (2**0.5) * attenuation[i].max()
+                attenuation = 1.0 - attenuation
+                p = x.clone()
+                x = alpha * x + noise_sampler(sigmas[i], sigmas[i + 1]) * attenuation * s_noise * sigma_up
+            else:
+                p = x.clone()
+                x = alpha * x + noise_sampler(sigmas[i], sigmas[i + 1]) * s_noise * sigma_up
  
     return x
+
+@torch.no_grad()
+def sample_euler_ancestral_na(model, x, sigmas, extra_args=None, callback=None, disable=None, eta=1., s_noise=1., noise_sampler=None):
+    return sample_euler_ancestral(model, x, sigmas, extra_args=extra_args, callback=callback, disable=disable, eta=eta, s_noise=s_noise, noise_sampler=noise_sampler, noise_attenuation=True)
 
 
 @torch.no_grad()
