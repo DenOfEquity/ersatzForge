ace7b533e9d60e0b1dea42059726cccdc2105a66
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2025-12-04 11:44:58 +0000

    update Z-Image-Turbo Control extension
    add cache to avoid reload VAE if image unchanged
    +tidy

--- extensions-builtin/ersatz_ControlOther/scripts/zimage.py
+++ extensions-builtin/ersatz_ControlOther/scripts/zimage.py
@@ -1,18 +1,18 @@
 import gradio
 import torch
 import numpy
-from modules.api.api import decode_base64_to_image
+from einops import rearrange
 
 from modules import scripts, shared
+from modules.api.api import decode_base64_to_image
 from modules.ui_components import InputAccordion, ToolButton
 from modules.sd_samplers_common import images_tensor_to_samples
 from backend.misc.image_resize import adaptive_resize
 
-from einops import rearrange, repeat
 
 class ersatzZImageTurboControl(scripts.Script):
     sorting_priority = 0
-    latent = None
+    last_image_hash = None
 
     def title(self):
         return "Z-Image-Turbo Control"
@@ -27,8 +27,8 @@ class ersatzZImageTurboControl(scripts.Script):
                 with gradio.Column():
                     zitc_image1 = gradio.Image(show_label=False, type="pil", height=300, sources=["upload", "clipboard"])
                 with gradio.Column():
-                    image_str = gradio.Slider(value=0.8, minimum=0.0, maximum=2.0, step=0.01, label="strength")
-                    image_stop = gradio.Slider(value=0.85, minimum=0.0, maximum=1.0, step=0.01, label="stop sigma")
+                    image_str = gradio.Slider(value=1.0, minimum=0.0, maximum=2.0, step=0.01, label="strength")
+                    image_stop = gradio.Slider(value=0.75, minimum=0.0, maximum=1.0, step=0.01, label="stop sigma")
                     with gradio.Row():
                         image_info = gradio.Textbox(value="", show_label=False, interactive=False, max_lines=1)
                         image_send = ToolButton(value="\U0001F4D0", interactive=False, variant="tertiary")
@@ -83,48 +83,60 @@ class ersatzZImageTurboControl(scripts.Script):
                 return
 
             x = kwargs["x"]
-            n, c, h, w = x.size()
-            input_device = x.device
-            input_dtype = x.dtype
+            
+            def calc_extra_mem(latent):
+                return latent.shape[0] * latent.shape[1] * latent.shape[2] * latent.element_size() * 1024
 
-            if isinstance (image, str):
-                k_image = decode_base64_to_image(image).convert("RGB")
+            this_image_hash = hash(str(list(image.getdata(band=None))).encode("utf-8"))
+
+            if this_image_hash == self.last_image_hash:
+                print ("[Z-Image-Turbo Control] used cache")
+                shared.ZITstrength = strength
+                shared.ZITstop = stop
+                extra_mem = calc_extra_mem(shared.ZITlatent)
             else:
-                k_image = image.convert("RGB")
+                self.last_image_hash = this_image_hash
 
-            k_image = numpy.array(k_image) / 255.0
-            k_image = numpy.transpose(k_image, (2, 0, 1))
-            k_image = torch.tensor(k_image).unsqueeze(0)
+                _, c, h, w = x.size()
+                input_device = x.device
+                input_dtype = x.dtype
 
-            # only go through the resize process if image is not already desired size
-            k_width = w * 8
-            k_height = h * 8
+                if isinstance (image, str):
+                    k_image = decode_base64_to_image(image).convert("RGB")
+                else:
+                    k_image = image.convert("RGB")
 
-            if k_image.shape[3] != k_width or k_image.shape[2] != k_height:
-                print ("[Z-Image-Turbo Control] resizing and center-cropping input to: ", k_width, k_height)
-                k_image = adaptive_resize(k_image, k_width, k_height, "lanczos", "center")
-            else:
-                print ("[Z-Image-Turbo Control] no image resize needed")
+                k_image = numpy.array(k_image) / 255.0
+                k_image = numpy.transpose(k_image, (2, 0, 1))
+                k_image = torch.tensor(k_image).unsqueeze(0)
 
-            k_latent = images_tensor_to_samples(k_image, None, None)
+                # only go through the resize process if image is not already desired size
+                k_width = w * 8
+                k_height = h * 8
 
-            # pad if needed - latent width and height must be multiple of 2
-            # could just adjust the resize to be *16, but the padding might be better for images that need only one extra row/col
-            patch_size = 2
-            pad_h = k_latent.shape[2] % patch_size
-            pad_w = k_latent.shape[3] % patch_size
-            k_latent = torch.nn.functional.pad(k_latent, (0, pad_w, 0, pad_h), mode="circular")
+                if k_image.shape[3] != k_width or k_image.shape[2] != k_height:
+                    print ("[Z-Image-Turbo Control] resizing and center-cropping input to: ", k_width, k_height)
+                    k_image = adaptive_resize(k_image, k_width, k_height, "lanczos", "center")
+                else:
+                    print ("[Z-Image-Turbo Control] no image resize needed")
 
-            latentH = k_latent.shape[2]
-            latentW = k_latent.shape[3]
-            extra_mem = n * k_latent.shape[1] * k_latent.shape[2] * k_latent.shape[3] * x.element_size() * 1024
+                k_latent = images_tensor_to_samples(k_image, None, None)
 
-            k_latent = rearrange(k_latent, 'b c (h ph) (w pw) -> b (h w) (ph pw c)', ph=2, pw=2)
-            k_latent = k_latent.to(input_device, input_dtype)
+                # pad if needed - latent width and height must be multiple of 2
+                # could just adjust the resize to be *16, but the padding might be better for images that need only one extra row/col
+                patch_size = 2
+                pad_h = k_latent.shape[2] % patch_size
+                pad_w = k_latent.shape[3] % patch_size
+                k_latent = torch.nn.functional.pad(k_latent, (0, pad_w, 0, pad_h), mode="circular")
 
-            shared.ZITlatent = k_latent
-            shared.ZITstrength = strength
-            shared.ZITstop = stop
+                k_latent = rearrange(k_latent, 'b c (h ph) (w pw) -> b (h w) (ph pw c)', ph=2, pw=2)
+                k_latent = k_latent.to(input_device, input_dtype)
+
+                shared.ZITlatent = k_latent
+                shared.ZITstrength = strength
+                shared.ZITstop = stop
+
+                extra_mem = calc_extra_mem(k_latent)
 
             print ("[Z-Image-Turbo Control] reserving extra memory (MB):", round(extra_mem/(1024*1024), 2))
             params.sd_model.forge_objects.unet.extra_preserved_memory_during_sampling = extra_mem
@@ -132,9 +144,11 @@ class ersatzZImageTurboControl(scripts.Script):
         return
 
     def postprocess(self, params, processed, *args):
-        shared.ZITlatent = None # if don't clear, can avoid VAE encode if image not changed
-        shared.ZITstrength = 0.0
-        shared.ZITstop = 1.0
-        params.sd_model.forge_objects.unet.extra_preserved_memory_during_sampling = 0
+        enabled = args[0]
+        if enabled:
+            # shared.ZITlatent = None # if don't clear, can avoid VAE encode if image not changed
+            shared.ZITstrength = 0.0
+            shared.ZITstop = 0.0
+            params.sd_model.forge_objects.unet.extra_preserved_memory_during_sampling = 0
 
         return
