9c13cafee93b12051e70f56a100decae7c760ba5
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2026-04-20 19:39:33 +0100

    simplify

--- modules/paths.py
+++ modules/paths.py
@@ -1,27 +1,5 @@
-import os
 import sys
-from modules.paths_internal import models_path, script_path, data_path, extensions_dir, extensions_builtin_dir, cwd  # noqa: F401
+from modules.paths_internal import data_path, models_path, script_path
 
 
 sys.path.insert(0, script_path)
-
-sd_path = os.path.dirname(__file__)
-
-path_dirs = [
-    (os.path.join(sd_path, '../packages_3rdparty'), 'gguf/quants.py', 'packages_3rdparty', []),
-    (os.path.join(sd_path, '../repositories/huggingface_guess'), 'huggingface_guess/detection.py', 'huggingface_guess', []),
-]
-
-paths = {}
-
-for d, must_exist, what, options in path_dirs:
-    must_exist_path = os.path.abspath(os.path.join(script_path, d, must_exist))
-    if not os.path.exists(must_exist_path):
-        print(f"Warning: {what} not found at path {must_exist_path}", file=sys.stderr)
-    else:
-        d = os.path.abspath(d)
-        if "atstart" in options:
-            sys.path.insert(0, d)
-        else:
-            sys.path.append(d)
-        paths[what] = d
