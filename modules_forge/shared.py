2fcebea3628e04bf05b6ff8005610b9095fb9990
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2026-04-22 15:54:49 +0100

    union

--- modules_forge/shared.py
+++ modules_forge/shared.py
@@ -3,6 +3,7 @@ import argparse
 
 from backend import utils
 from modules.paths_internal import models_path
+from modules import shared
 from pathlib import Path
 
 
@@ -55,12 +56,43 @@ def add_supported_control_model(control_model):
     return
 
 
+# caching by Haoming02, ForgeNeo
+from collections import OrderedDict
+
+class ModelCache:
+    def __init__(self):
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return None
+
+    def put(self, key, model):
+        self.cache[key] = model
+        self.cache.move_to_end(key)
+
+        max_size = max(1, shared.opts.data.get("control_net_model_cache_size", 3))
+        while len(self.cache) > max_size:
+            self.cache.popitem(last=False)
+
+_CONTROL_MODEL_CACHE = ModelCache()
+
+
 def try_load_supported_control_model(ckpt_path):
     global supported_control_models
-    state_dict = utils.load_torch_file(ckpt_path, safe_load=True)
-    for supported_type in supported_control_models:
-        state_dict_copy = dict(state_dict)
-        model = supported_type.try_build_from_state_dict(state_dict_copy, ckpt_path)
-        if model is not None:
-            return model
-    return None
+    global _CONTROL_MODEL_CACHE
+
+    control_model = _CONTROL_MODEL_CACHE.get(ckpt_path)
+
+    if control_model is None:
+        state_dict = utils.load_torch_file(ckpt_path, safe_load=True)
+        for supported_type in supported_control_models:
+            state_dict_copy = dict(state_dict)
+            control_model = supported_type.try_build_from_state_dict(state_dict_copy, ckpt_path)
+            if control_model is not None:
+                _CONTROL_MODEL_CACHE.put(ckpt_path, control_model)
+                break
+
+    return control_model
