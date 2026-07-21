9978c5599a68104b17dbda4a6501f67599ab4828
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2026-07-21 01:20:02 +0100

    prompt_matrix: tidy
    remove unused function
    only create grid if >1 image generated

--- scripts/prompt_matrix.py
+++ scripts/prompt_matrix.py
@@ -5,34 +5,6 @@ from modules.processing import process_images
 from modules.shared import opts, state
 
 
-def draw_xy_grid(xs, ys, x_label, y_label, cell):
-    res = []
-
-    ver_texts = [[images.GridAnnotation(y_label(y))] for y in ys]
-    hor_texts = [[images.GridAnnotation(x_label(x))] for x in xs]
-
-    first_processed = None
-
-    state.job_count = len(xs) * len(ys)
-
-    for iy, y in enumerate(ys):
-        for ix, x in enumerate(xs):
-            state.job = f"{ix + iy * len(xs) + 1} out of {len(xs) * len(ys)}"
-
-            processed = cell(x, y)
-            if first_processed is None:
-                first_processed = processed
-
-            res.append(processed.images[0])
-
-    grid = images.image_grid(res, rows=len(ys))
-    grid = images.draw_grid_annotations(grid, res[0].width, res[0].height, hor_texts, ver_texts)
-
-    first_processed.images = [grid]
-
-    return first_processed
-
-
 class Script(scripts.Script):
     def title(self):
         return "Prompt matrix"
@@ -66,7 +38,9 @@ class Script(scripts.Script):
 
         all_prompts = []
         prompt_matrix_parts = original_prompt.split("|")
-        combination_count = 2 ** (len(prompt_matrix_parts) - 1)
+        last_part_index = len(prompt_matrix_parts) - 1
+
+        combination_count = 2 ** last_part_index
         for combination_num in range(combination_count):
             selected_prompts = [text.strip().strip(",") for n, text in enumerate(prompt_matrix_parts[1:]) if combination_num & (1 << n)]
 
@@ -77,27 +51,29 @@ class Script(scripts.Script):
 
             all_prompts.append(delimiter.join(selected_prompts))
 
-        p.n_iter = len(all_prompts)
+        prompt_count = len(all_prompts)
+        p.n_iter = prompt_count
         p.batch_size = 1 # cond padding will change results
 
         p.do_not_save_grid = True
 
-        print(f"[Prompt matrix] will generate {len(all_prompts)} images in {p.n_iter} batches.")
+        print(f"[Prompt matrix] will generate {p.n_iter} images.")
 
         if prompt_type == "positive":
             p.prompt = all_prompts
         else:
             p.negative_prompt = all_prompts
-        p.seed = [p.seed + (i if different_seeds else 0) for i in range(len(all_prompts))]
+        p.seed = [p.seed + (i if different_seeds else 0) for i in range(prompt_count)]
         processed = process_images(p)
 
-        grid = images.image_grid(processed.images, p.batch_size, rows=1 << ((len(prompt_matrix_parts) - 1) // 2))
-        grid = images.draw_prompt_matrix(grid, processed.images[0].width, processed.images[0].height, prompt_matrix_parts, margin_size)
-        processed.images.insert(0, grid)
-        processed.index_of_first_image = 1
-        processed.infotexts.insert(0, processed.infotexts[0])
+        if prompt_count > 1:
+            grid = images.image_grid(processed.images, p.batch_size, rows=1 << (last_part_index // 2))
+            grid = images.draw_prompt_matrix(grid, processed.images[0].width, processed.images[0].height, prompt_matrix_parts, margin_size)
+            processed.images.insert(0, grid)
+            processed.index_of_first_image = 1
+            processed.infotexts.insert(0, processed.infotexts[0])
 
-        if opts.grid_save:
-            images.save_image(processed.images[0], p.outpath_grids, "prompt_matrix", extension=opts.grid_format, prompt=original_prompt, seed=processed.seed, grid=True, p=p)
+            if opts.grid_save:
+                images.save_image(processed.images[0], p.outpath_grids, "prompt_matrix", extension=opts.grid_format, prompt=original_prompt, seed=processed.seed, grid=True, p=p)
 
         return processed
