cf9b236e0f32cf363a500e442c50b485a22d0f64
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2026-08-14 00:12:10 +0100

    anima 2.9B lora auto-conversion

--- extensions-builtin/sd_forge_lora/networks.py
+++ extensions-builtin/sd_forge_lora/networks.py
@@ -21,6 +21,51 @@ def load_lora_for_models(model, clip, lora, strength_model, strength_clip, filen
             elif k.startswith("lora_unet_llm_adapter"):
                 lora[k.replace("lora_unet_llm_adapter", "lora_te_llm_adapter", 1)] = lora.pop(k)
 
+        if model.model.diffusion_model.num_blocks == 40: # model is 2.9B variant
+            from packages.huggingface_guess.detection import count_blocks
+
+            lora_blocks_count_1 = count_blocks(lora, "lora_unet_blocks_" + "{}_")
+            lora_blocks_count_2 = count_blocks(lora, "diffusion_model.blocks." + "{}.")
+            if 0 < lora_blocks_count_1 <= 28 or 0 < lora_blocks_count_2 <= 28: # lora is for standard Anima
+                if lora_blocks_count_1:
+                    prefix = "lora_unet_blocks_"
+                else:
+                    prefix = "diffusion_model.blocks."
+                split_c = prefix[-1]
+                len_prefix = len(prefix)
+
+                keys = list(lora.keys())
+
+                if 1: # method 0: adjust block indices with duplication, seems slightly better
+                    lora_copy = lora.copy()
+                    MAPPING = {
+                         "0":[0],        "1":[1, 2],     "2":[3],        "3":[4, 5],
+                         "4":[6],        "5":[7, 8],     "6":[9],        "7":[10, 11],
+                         "8":[12],       "9":[13, 14],  "10":[15],      "11":[16, 17],
+                        "12":[18],      "13":[19],      "14":[20, 21],  "15":[22],
+                        "16":[23, 24],  "17":[25],      "18":[26, 27],  "19":[28],
+                        "20":[29, 30],  "21":[31],      "22":[32, 33],  "23":[34],
+                        "24":[35, 36],  "25":[37],      "26":[38],      "27":[39]
+                    }
+                    for k in keys:
+                        if k.startswith(prefix):
+                            bne = k.index(split_c, len_prefix)
+                            bn = k[len_prefix:bne]          # lora block number as string
+                            for m in MAPPING[bn]:
+                                lora[f"{prefix}{m}{k[bne:]}"] = lora_copy[k].clone()
+                    del lora_copy
+                else: # method 1: adjust block indices to new positions only
+                    new_lora = {}
+                    MAPPING = [0, 1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16, 18, 19, 20, 22, 23, 25, 26, 28, 29, 31, 32, 34, 35, 37, 38, 39]
+                    for k in keys:
+                        if k.startswith(prefix):
+                            bne = k.index(split_c, len_prefix)
+                            bn = int(k[len_prefix:bne])     # lora block number as integer
+                            new_lora[f"{prefix}{MAPPING[bn]}{k[bne:]}"] = lora[k].clone()
+                        else:
+                            new_lora[k] = lora[k].clone()
+                    lora = new_lora
+
     if model is not None:
         unet_keys = model_lora_keys_unet(model.model)
         lora_unet, lora = load_lora(lora, unet_keys)
