d4334d85de2174f46956eafa47120dbdee386545
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2026-08-24 17:45:12 +0100

    seed UI
    hmm

--- modules/processing_scripts/seed.py
+++ modules/processing_scripts/seed.py
@@ -1,11 +1,9 @@
+import gradio
 import json
 
-import gradio as gr
-
-from modules import scripts, ui, errors
-from modules.infotext_utils import PasteField
-from modules.ui_components import ToolButton
-from modules import infotext_utils
+from modules import scripts
+from modules.infotext_utils import parse_generation_parameters, PasteField
+from modules.ui_components import InputAccordion, ToolButton
 
 
 class ScriptSeed(scripts.ScriptBuiltinUI):
@@ -24,78 +22,69 @@ class ScriptSeed(scripts.ScriptBuiltinUI):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
-        with gr.Row(elem_id=self.elem_id("seed_row")):
-            self.seed = gr.Number(label='Seed', value=-1, minimum=-1, maximum=4294967295, elem_id=self.elem_id("seed"), min_width=100, precision=0)
+        with InputAccordion(False, label="Seed (checked: random)") as random:
+            with gradio.Row():
+                self.seed = gradio.Number(label="Seed", value=-1, minimum=-1, maximum=4294967295, precision=0, elem_id=self.elem_id("seed"), min_width=100)
 
-            random_seed = ToolButton(ui.random_symbol, elem_id=self.elem_id("random_seed"), tooltip="Set seed to -1, which will cause a new random number to be used every time")
-            reuse_seed = ToolButton(ui.reuse_symbol, elem_id=self.elem_id("reuse_seed"), tooltip="Reuse seed from last generation, mostly useful if it was randomized")
+                subseed = gradio.Number(label="Variation seed", value=-1, minimum=-1, maximum=4294967295, precision=0, scale=0, elem_id=self.elem_id("subseed"))
+                subseed_strength = gradio.Number(label="Variation strength", value=0, minimum=0, maximum=1, step=0.01, scale=0, elem_id=self.elem_id("subseed_strength"))
+                random_seed = ToolButton("\U0001f3b2\ufe0f", elem_id=self.elem_id("random_seed"), tooltip="set Seed (and Variation) to -1, which means random values will be used") # 🎲️
+                reuse_seed = ToolButton("\u267b\ufe0f", elem_id=self.elem_id("reuse_seed"), tooltip="reuse Seed (and Variation) from last generation") # ♻️
 
-            seed_checkbox = gr.Checkbox(label='Extra', elem_id=self.elem_id("subseed_show"), value=False, scale=0, min_width=60)
+            with gradio.Row():
+                seed_resize_from_w = gradio.Number(label="Resize from width", value=0, minimum=0, maximum=2048, step=8, precision=0, elem_id=self.elem_id("seed_resize_from_w"))
+                seed_resize_from_h = gradio.Number(label="Resize from height", value=0, minimum=0, maximum=2048, step=8, precision=0, elem_id=self.elem_id("seed_resize_from_h"))
 
-        with gr.Group(visible=False, elem_id=self.elem_id("seed_extras")) as seed_extras:
-            with gr.Row(elem_id=self.elem_id("subseed_row")):
-                subseed = gr.Number(label='Variation seed', value=-1, elem_id=self.elem_id("subseed"), precision=0)
-                random_subseed = ToolButton(ui.random_symbol, elem_id=self.elem_id("random_subseed"))
-                reuse_subseed = ToolButton(ui.reuse_symbol, elem_id=self.elem_id("reuse_subseed"))
-                subseed_strength = gr.Slider(label='Variation strength', value=0.0, minimum=0, maximum=1, step=0.01, elem_id=self.elem_id("subseed_strength"))
 
-            with gr.Row(elem_id=self.elem_id("seed_resize_from_row")):
-                seed_resize_from_w = gr.Slider(minimum=0, maximum=2048, step=8, label="Resize seed from width", value=0, elem_id=self.elem_id("seed_resize_from_w"))
-                seed_resize_from_h = gr.Slider(minimum=0, maximum=2048, step=8, label="Resize seed from height", value=0, elem_id=self.elem_id("seed_resize_from_h"))
-
-        random_seed.click(fn=lambda: -1, show_progress="hidden", inputs=None, outputs=self.seed)
-        random_subseed.click(fn=lambda: -1, show_progress="hidden", inputs=None, outputs=subseed)
-
-        seed_checkbox.change(lambda x: gr.update(visible=x), show_progress="hidden", inputs=[seed_checkbox], outputs=[seed_extras])
+        random_seed.click(fn=lambda: (-1, -1), inputs=None, outputs=[self.seed, subseed], show_progress="hidden")
 
         self.infotext_fields = [
             PasteField(self.seed, "Seed", api="seed"),
-            PasteField(seed_checkbox, lambda d: "Variation seed" in d or "Seed resize from-1" in d),
             PasteField(subseed, "Variation seed", api="subseed"),
             PasteField(subseed_strength, "Variation seed strength", api="subseed_strength"),
             PasteField(seed_resize_from_w, "Seed resize from-1", api="seed_resize_from_h"),
             PasteField(seed_resize_from_h, "Seed resize from-2", api="seed_resize_from_w"),
         ]
 
-        self.on_after_component(lambda x: connect_reuse_seed(self.seed, reuse_seed, x.component, False), elem_id=f'generation_info_{self.tabname}')
-        self.on_after_component(lambda x: connect_reuse_seed(subseed, reuse_subseed, x.component, True), elem_id=f'generation_info_{self.tabname}')
+        self.on_after_component(lambda x: connect_reuse_seed(self.seed, subseed, reuse_seed, x.component), elem_id=f'generation_info_{self.tabname}')
 
-        return self.seed, seed_checkbox, subseed, subseed_strength, seed_resize_from_w, seed_resize_from_h
+        return random, self.seed, subseed, subseed_strength, seed_resize_from_w, seed_resize_from_h
 
-    def setup(self, p, seed, seed_checkbox, subseed, subseed_strength, seed_resize_from_w, seed_resize_from_h):
-        p.seed = seed
+    def setup(self, p, random, seed, subseed, subseed_strength, seed_resize_from_w, seed_resize_from_h):
+        p.seed = -1 if random else seed
 
-        if seed_checkbox and subseed_strength > 0:
-            p.subseed = subseed
+        if subseed_strength > 0:
+            p.subseed = -1 if random else subseed
             p.subseed_strength = subseed_strength
 
-        if seed_checkbox and seed_resize_from_w > 0 and seed_resize_from_h > 0:
+        if seed_resize_from_w > 0 and seed_resize_from_h > 0:
             p.seed_resize_from_w = seed_resize_from_w
             p.seed_resize_from_h = seed_resize_from_h
 
 
-def connect_reuse_seed(seed: gr.Number, reuse_seed: gr.Button, generation_info: gr.Textbox, is_subseed):
+def connect_reuse_seed(seed: gradio.Number, subseed: gradio.Number, reuse_seed: ToolButton, generation_info: gradio.Textbox):
     """ Connects a 'reuse (sub)seed' button's click event so that it copies last used
         (sub)seed value from generation info the to the seed field. If copying subseed and subseed strength
         was 0, i.e. no variation seed was used, it copies the normal seed value instead."""
 
     def copy_seed(gen_info_string: str, index):
-        res = -1
+        s = -1
+        ss = -1
         try:
             gen_info = json.loads(gen_info_string)
-            infotext = gen_info.get('infotexts')[index]
-            gen_parameters = infotext_utils.parse_generation_parameters(infotext, [])
-            res = int(gen_parameters.get('Variation seed' if is_subseed else 'Seed', -1))
+            infotext = gen_info.get("infotexts")[index]
+            gen_parameters = parse_generation_parameters(infotext, [])
+            s = int(gen_parameters.get("Seed", -1))
+            ss = int(gen_parameters.get("Variation seed", -1))
         except Exception:
-            if gen_info_string:
-                errors.report(f"Error retrieving seed from generation info: {gen_info_string}", exc_info=True)
+            pass
 
-        return [res, gr.skip()]
+        return s, ss
 
     reuse_seed.click(
         fn=copy_seed,
         js="(x, y) => [x, selected_gallery_index()]",
         inputs=[generation_info, seed],
-        outputs=[seed, seed],
+        outputs=[seed, subseed],
         show_progress="hidden",
     )
